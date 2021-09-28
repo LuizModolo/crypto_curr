3329b08350029d9c3cd70bcb38fcbbc13b91be55
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-09-28 08:40:20 -0300

    Main style + JScode (home) - Mobile First

---
+++ script.js
@@ -0,0 +1,71 @@
+const getApi = async () => {
+  const response = await fetch(' https://api.coinlore.net/api/tickers/').then(response => response.json());
+  const response2 = await fetch('https://api.coinlore.net/api/coin/markets/?id=90').then(response => response.json());
+  const response3 = await fetch('https://api.coinlore.net/api/global/').then(response => response.json());
+  return { coins: response, exchanges: response2, global: response3 };
+}
+
+
+function createSection(main) {
+  const newSection = document.createElement('section');
+  newSection.className = 'coin-section';
+  main.appendChild(newSection);
+  return newSection
+}
+
+function createRank(section, rank, title) {
+  const newSpan = document.createElement('span');
+  newSpan.innerText = rank;
+  if ( parseFloat(rank) < 0) {
+    newSpan.style.color = 'red';
+    newSpan.style.fontWeight = '900';
+  } else if ((title === '1h' || title ==='24h' || title ==='7d') && parseFloat(rank) > 0){
+    newSpan.style.fontWeight = '900';
+    newSpan.style.color = 'green';
+  }
+  section.appendChild(newSpan);
+}
+
+function createMainContent(coins, key, main, title) {
+  const newSection = createSection(main);
+  const titleSpan = document.createElement('span');
+  titleSpan.className = 'title-span';
+  titleSpan.innerText = title;
+  newSection.appendChild(titleSpan);
+  coins.data.forEach(coin => {
+    createRank(newSection, coin[key], title);
+  });
+}
+
+const fillSections = async () => {
+  const { coins } = await getApi();
+  console.log(coins)
+  const main = document.querySelector('.main-content');
+  const keyArray = ['rank', 'symbol', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price_usd'];
+  const titleArray = ['Rank', 'Símbolo', '1h', '24h', '7d', 'Preço(USD)']
+  keyArray.forEach((key, i) => {
+    createMainContent(coins, key, main, titleArray[i]);
+  });
+  loadingRemove();
+}
+
+function loadingScreen() {
+  const newSection = document.createElement('section');  
+  const nav = document.querySelector('.nav-bar');
+  newSection.className = 'loading-container';
+  document.querySelector('body').insertBefore(newSection, nav);
+  const loadImage = document.createElement('img'); 
+  loadImage.className = 'load-image';
+  loadImage.src = './imgs/cc_logo_transp.png';
+  newSection.appendChild(loadImage);
+}
+
+function loadingRemove() {
+  const section = document.querySelector('.loading-container');
+  section.remove();
+}
+
+window.onload = () => {
+  loadingScreen();
+  fillSections();  
+}
